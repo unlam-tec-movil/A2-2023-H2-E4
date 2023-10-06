d9df9d858254a5f4a6d468802d0e1ccda05b11b2
Andres Calizaya <andres.calizaya@naranjax.com>
2023-10-06 00:26:32 -0300

    fix lint

--- app/src/main/java/ar/edu/unlam/mobile/scaffold/ui/theme/Type.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/ui/theme/Type.kt
@@ -7,17 +7,14 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
-val Typography =
-Typography(
-    bodyLarge =
-        TextStyle(
+val Typography = Typography(
+    bodyLarge = TextStyle(
             fontFamily = FontFamily.Default,
             fontWeight = FontWeight.Normal,
             fontSize = 16.sp,
             lineHeight = 24.sp,
             letterSpacing = 0.5.sp,
         ),
-    /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
@@ -32,5 +29,9 @@ Typography(
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
+)
+    /* Other default text styles to override
+
+
      */
-    )
+
