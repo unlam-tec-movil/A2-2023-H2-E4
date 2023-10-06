80a1fbb6dee125b341fcc47d81d8a15e600ac03b
Andres Calizaya <andres.calizaya@naranjax.com>
2023-10-06 00:31:49 -0300

    fix lint

--- app/src/main/java/ar/edu/unlam/mobile/scaffold/ui/theme/Type.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/ui/theme/Type.kt
@@ -14,7 +14,7 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp,
-        ),
+    ),
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
