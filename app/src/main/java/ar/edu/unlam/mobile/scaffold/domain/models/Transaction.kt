11a0dc6f41b743a512c86a4d673c6ff322ddbd5d
Andres Calizaya <andres.calizaya@naranjax.com>
2023-10-07 20:53:01 -0300

    Se crean los models

--- app/src/main/java/ar/edu/unlam/mobile/scaffold/domain/models/Transaction.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/domain/models/Transaction.kt
@@ -5,6 +5,8 @@ import java.time.LocalDate
 data class Transaction(
     val type: TransactionType,
     val amount: Float,
+//    val currency: Currency, ToDo: Implementar Currency
     val category: Category,
+    val description: String,
     val date: LocalDate,
 )
