fb83a5b2bd155efe4dd3faccc6915ea2064f4196
Andres Calizaya <andres.calizaya@naranjax.com>
2023-10-07 20:53:01 -0300

    Se crean los models

--- app/src/main/java/ar/edu/unlam/mobile/scaffold/domain/models/Category.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/domain/models/Category.kt
@@ -1,6 +1,7 @@
 package ar.edu.unlam.mobile.scaffold.domain.models
 data class Category(
     val id: Int,
-    val nombre: String,
-    val icon: String,
+    val type: TransactionType,
+    val name: String,
+    val color: String,
 )
