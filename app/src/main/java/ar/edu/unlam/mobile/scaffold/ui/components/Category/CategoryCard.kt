28d734740ee6364c1fa1226ddc83b9ee9628c4c8
Gabriel Ricardes <114497273+Gaburiiru@users.noreply.github.com>
2023-10-07 21:13:10 -0300

    creacion de screen categoria

--- app/src/main/java/ar/edu/unlam/mobile/scaffold/ui/components/Category/CategoryCard.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/ui/components/Category/CategoryCard.kt
@@ -1,3 +1,5 @@
+@file:Suppress("ASSIGNED_BUT_NEVER_ACCESSED_VARIABLE")
+
 package ar.edu.unlam.mobile.scaffold.ui.components.Category
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -5,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -24,70 +27,86 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.*
 
 @Composable
 fun CategoryCard() {
-    Card(
+    Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-            .background(Color.Transparent)
+            .fillMaxSize()
+            .background(Color.White),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Box(
+        Card(
             modifier = Modifier
                 .fillMaxWidth()
-                .background(Color.Gray),
-            contentAlignment = Alignment.Center // Centratodo el contenido verticalmente
-        ){
-            Column(
+                .wrapContentHeight()
+                .background(Color.Transparent)
+        ) {
+            Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally // Centra el contenido horizontalmente
+                    .background(Color.Gray),
+                contentAlignment = Alignment.Center
             ){
-                Text(
-                    text = "Categorías",
-                    color = Color.White,
-                    fontSize = 25.sp
-                )
-
-                Spacer(modifier = Modifier.height(16.dp)) // Agrega espacio vertical
-
-                var text by remember { mutableStateOf("Ingrese una categoría") }
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center // Centra el contenido horizontalmente
-                ) {
-                    BasicTextField(
-                        value = text,
-                        onValueChange = {
-                            text = it
-                        },
-                        modifier = Modifier
-                            .weight(1f) // Ocupa el espacio restante horizontalmente
-                            .height(40.dp)
-                            .padding(
-                                start = 10.dp,
-                                end = 10.dp
-                            )
-                            .background(
-                                Color.White,
-                                shape = RoundedCornerShape(10.dp))
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Text(
+                        text = "Categorías",
+                        color = Color.White,
+                        fontSize = 25.sp
                     )
-                    Button(
-                        onClick = { /*TODO*/ },
-                        colors = ButtonDefaults.buttonColors(Color.Blue),
-                        modifier = Modifier
-                            .size(40.dp)
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Row(
+                        Modifier.fillMaxWidth(), Arrangement.Center
                     ) {
-                        Text(
-                            text = "+",
-                            color = Color.White,
-                            fontSize = 10.sp
+                        var category by remember {
+                            mutableStateOf(value="")
+                        }
+                        TextField(
+                            value = category,
+                            onValueChange = {
+                                category = it
+                            },
+                            placeholder = { Text("Ingresa una categoria") },
+                            modifier = Modifier
+                                .weight(1f)
+                                .height(40.dp)
+                                .padding(
+                                    start = 10.dp,
+                                    end = 10.dp,
+                                )
+                                .background(
+                                    Color.White,
+                                    shape = RoundedCornerShape(10.dp),
+                                )
                         )
+                        Button(
+                            onClick = { /*TODO*/ },
+                            colors = ButtonDefaults.buttonColors(Color.Blue),
+                            modifier = Modifier
+                                .size(40.dp)
+                        ) {
+                            Text(
+                                text = "+",
+                                color = Color.White,
+                                fontSize = 20.sp,
+                            )
+                        }
                     }
                 }
             }
