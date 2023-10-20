0cba762c7e186cbbdee916beb0dfbc950803ad55
MarielML <marielmarmol1511@outlook.com>
2023-10-20 09:23:19 -0300

    correccion

--- app/src/main/java/ar/edu/unlam/mobile/scaffold/data/category/Category.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/data/category/Category.kt
@@ -4,7 +4,7 @@ import androidx.compose.ui.graphics.Color
 import ar.edu.unlam.mobile.scaffold.domain.models.TransactionType
 
 data class Category(
-    var id: Int = 0
+    var id: Int = 0,
     var type: TransactionType = TransactionType.EXPENSE,
     var name: String = "",
     var totalAmount: Double = 0.0,
