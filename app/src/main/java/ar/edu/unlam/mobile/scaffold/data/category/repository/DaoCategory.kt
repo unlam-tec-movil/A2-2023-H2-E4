fa39ff5741415b1e3fcc95fdb705ae011966dd04
Gabriel Ricardes <114497273+Gaburiiru@users.noreply.github.com>
2023-10-30 00:28:54 -0300

    implementacion de Query dao para filtrar por type

--- app/src/main/java/ar/edu/unlam/mobile/scaffold/data/category/repository/DaoCategory.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/data/category/repository/DaoCategory.kt
@@ -5,11 +5,18 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
+import ar.edu.unlam.mobile.scaffold.domain.models.TransactionType
 
 @Dao
 interface DaoCategory {
+
+    // trae todas las categorias
     @Query("SELECT * FROM Category")
     fun getAllCategories(): List<CategoryEntity>
+
+    // filtra por type de categorias
+    @Query("SELECT * FROM Category WHERE type = :transactionType")
+    fun getCategoriesByType(transactionType: TransactionType): List<CategoryEntity>
 
     @Insert
     fun insertCategory(category: CategoryEntity): Long
