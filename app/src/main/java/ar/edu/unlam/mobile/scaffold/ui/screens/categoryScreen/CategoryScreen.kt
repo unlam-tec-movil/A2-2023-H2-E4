669efc987241ceca6de70d9bc1c7717aa1ba995d
Andres Calizaya <andres.calizaya@naranjax.com>
2023-11-01 23:56:41 -0300

    Se verifica que se agregue bien a la BD las categorias

--- app/src/main/java/ar/edu/unlam/mobile/scaffold/ui/screens/categoryScreen/CategoryScreen.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/ui/screens/categoryScreen/CategoryScreen.kt
@@ -5,11 +5,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,6 +21,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -33,7 +39,7 @@ fun CategoryScreen(
     var selectedColor by remember { mutableStateOf<ColorsCategory?>(null) }
     var colorClick by remember { mutableStateOf(false) }
     var selectedOption by remember { mutableStateOf("Ingresos") }
-    var category by remember { mutableStateOf("") }
+    var categoryName by remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier
@@ -48,12 +54,21 @@ fun CategoryScreen(
         )
         Spacer(modifier = Modifier.height(16.dp))
 
-        // Componente TextField
-        CategoryTextField(
-            colorClick = colorClick,
-            category = category,
-            onCategoryChanged = { updatedCategory ->
-                category = updatedCategory
+        TextField(
+            value = categoryName,
+            onValueChange = { categoryName = it },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 10.dp, end = 10.dp),
+            placeholder = {
+                Text(
+                    text = "Ingrese el nombre de una categoria",
+                    fontSize = 15.sp,
+                    color = if (!colorClick) Color.Gray else Color.Red,
+                )
             },
         )
 
@@ -73,14 +88,15 @@ fun CategoryScreen(
         }
 
         // Validación que habilita el botón
-        val isInputValid = category.isNotBlank() && selectedColor != null
+        val isInputValid = categoryName.isNotBlank() && selectedColor != null
 
         Button(
             enabled = isInputValid,
             onClick = {
                 // Llama funcion de viewModel que hace el insert a la BD
-                viewModel.addCategoryToDatabase(category, selectedOption, selectedColor?.colorHex ?: ColorsCategory.ROJO.colorHex)
-                category = ""
+                viewModel.addCategoryToDatabase(categoryName, selectedOption, selectedColor?.colorHex ?: ColorsCategory.ROJO.colorHex)
+                categoryName = ""
+
             },
         ) {
             Text(text = "Agregar")
