ba57cc844f9758cc0cdaabba079f93fee49b2d8e
Andres Calizaya <andres.calizaya@naranjax.com>
2023-11-07 20:15:29 -0300

    Added comment

--- app/src/main/java/ar/edu/unlam/mobile/scaffold/data/app/local/core/transaction/TransactionRepository.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/data/app/local/core/transaction/TransactionRepository.kt
@@ -1,11 +1,13 @@
 package ar.edu.unlam.mobile.scaffold.data.app.local.core.transaction
 
+import ar.edu.unlam.mobile.scaffold.domain.models.Transaction
+
 class TransactionRepository(private val daoTransaction: DaoTransaction) : TransactionRepositoryInterface {
     override suspend fun addTransaction(transaction: TransactionEntity) {
         daoTransaction.insertTransaction(transaction)
     }
 
-    override fun getAllTransactions(): List<TransactionEntity> {
-        return daoTransaction.getAllTransactions()
+    override suspend fun getAllTransactions(): List<Transaction> {
+        return daoTransaction.getAllTransactions() //Aca transformar entity to model
     }
 }
